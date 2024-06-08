504581fc555ec2cededb7eb64419c738d095c72a
Dmitri Tikhomirov <dmitri.tikhomirov@gmail.com>
2024-06-08 22:48:42 +0300

    Updated sneak, updated keyboard controls, loot reduction WIP.

--- source/headers/fallout-2-adjustments.h
+++ source/headers/fallout-2-adjustments.h
@@ -1,1 +1,24 @@
 #define INI_PATH "mods\\fallout-2-adjustments.ini"
+
+#define PID_LEATHER_ARMOR            1
+#define PID_METAL_ARMOR              2
+#define PID_POWERED_ARMOR            3
+#define PID_COMBAT_ARMOR             17
+#define PID_LEATHER_JACKET           74
+#define PID_PURPLE_ROBE              113
+#define PID_HARDENED_POWER_ARMOR     232
+#define PID_BROTHERHOOD_COMBAT_ARMOR 239
+#define PID_TESLA_ARMOR              240
+#define PID_CURED_LEATHER_ARMOR      265
+#define PID_ADVANCED_POWER_ARMOR     348
+#define PID_ADVANCED_POWER_ARMOR_MK2 349
+#define PID_LEATHER_ARMOR_MK_II      379
+#define PID_METAL_ARMOR_MK_II        380
+#define PID_COMBAT_ARMOR_MK_II       381
+#define PID_BRIDGEKEEPERS_ROBE       524
+
+#define KEY_CODE_ESC      1
+#define KEY_CODE_I        23
+#define KEY_CODE_S        31
+#define KEY_CODE_TILDE    41
+#define KEY_CODE_DISABLED 256
